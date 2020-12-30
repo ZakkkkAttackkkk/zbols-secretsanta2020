d0d6806c8c3594a3b7760b2cc2b9269a6664f440
ZakkkkAttackkkk <zakjimenez@outlook.com>
2020-12-31 00:44:27 +0800

    Minified files

--- base.js
+++ base.js
@@ -1,352 +1,46 @@
-function bisect (arr, el) {
-    var a = 0, z = arr.length, m;
-    for(;;) {
-        m = Math.floor((a+z)/2);
-        if (arr[m] == el || a == z) return m;
-        if (arr[m] < el) a = m + 1;
-        else z = m;
-    }
-}
-
-class OptSet {
-    constructor () {
-        this.values = [];
-    }
-
-    add (data) {
-        var ind = bisect(this.values, data);
-        if (this.values[ind] != data || this.values[ind] == undefined) {
-            this.values.splice(ind,0,data);
-        }
-    }
-
-    remove (data) {
-        var ind = bisect(this.values, data);
-        if (this.values[ind] == data) {
-            this.values.splice(ind,1);
-        }
-    }
-
-    toggle (data) {
-        var ind = bisect(this.values, data);
-        if (this.values[ind] != data || this.values[ind] == undefined) {
-            this.values.splice(ind,0,data);
-        }
-        else {
-            this.values.splice(ind,1);
-        }
-    }
-
-    has (data) {
-        var ind = bisect(this.values, data);
-        return this.values[ind] == data;
-    }
-}
-
-class Trie {
-    constructor () {
-        this.data = undefined;
-        this.keys = [];
-        this.values = [];
-    }
-
-    set (key, data) {
-        if (key.length == 0) {
-            this.data = data;
-        }
-        else {
-            var k, ey;
-            [k, ...ey] = key;
-            var ind = bisect(this.keys, k);
-            if (this.keys[ind] != k || this.keys[ind] == undefined) {
-                this.keys.splice(ind,0,k);
-                this.values.splice(ind,0,new Trie());
-            }
-            this.values[ind].set(ey, data);
-        }
-    }
-
-    get (key) {
-        if (key.length == 0) {
-            return this.data;
-        }
-        else {
-            var k, ey;
-            [k, ...ey] = key;
-            var ind = bisect(this.keys, k);
-            if (this.keys[ind] != k || this.keys[ind] == undefined) {
-                return undefined;
-            }
-            return this.values[ind].get(ey);
-        }
-    }
-
-    forEach (fn, key = []) {
-        if (this.data !== undefined) fn(this.data, key);
-        this.values.forEach((trie, ind) => {
-            trie.forEach(fn, [...key,this.keys[ind]]);
-        })
-    }
-
-    entries () {
-        return {trie:this,
-            *[Symbol.iterator]() {
-            if (this.trie.data != undefined) {
-                yield [[], this.trie.data];
-            }
-            for (var i = 0; i < this.trie.keys.length; i++){
-                for (var [key, val] of this.trie.values[i].entries()) {
-                    yield [[this.trie.keys[i], ...key], val];
-                }
-            }
-        }}
-    }
-}
-
-class Animation {
-    ;
-}
-
-const GameEventTarget = {
-    constructor () {
-        this.passKeyDown = false;
-        this.passKeyUp = false;
-        this.passKeyPress = false;
-        this.passMouseDown = false;
-        this.passMouseUp = false;
-        this.passMousePress = false;
-    },
-
-    keydown (ev) {
-        console.log("keydown", this, ...arguments);
-        return this.passKeyDown;
-    },
-
-    keyup (ev) {
-        console.log("keyup", this, ...arguments);
-        return this.passKeyUp;
-    },
-
-    keypress (ev) {
-        console.log("keypress", this, ...arguments);
-        return this.passKeyPress;
-    },
-    
-    mousedown (ev) {
-        console.log("mousedown", this, ...arguments);
-        return this.passMouseDown;
-    },
-
-    mouseup (ev) {
-        console.log("mouseup", this, ...arguments);
-        return this.passMouseUp;
-    },
-
-    mousepress (ev) {
-        console.log("mousepress", this, ...arguments);
-        return this.passMousePress;
-    }
-}
-
-class Drawable {
-    constructor (ctx, x, y, w, h) {
-        this.ctx = ctx;
-        this._x = x;
-        this._y = y;
-        this._w = w;
-        this._h = h;
-    }
-
-    get x () {
-        return this._x;
-    }
-
-    set x (value) {
-        this._x = value;
-    }
-
-    get y () {
-        return this._y;
-    }
-
-    set y (value) {
-        this._y = value;
-    }
-
-    get w () {
-        return this._w;
-    }
-
-    set w (value) {
-        this._w = value;
-    }
-
-    get h () {
-        return this._h;
-    }
-
-    set h (value) {
-        this._h = value;
-    }
-
-    draw () {}
-}
-
-class DrawableGroup {
-    constructor (ctx, x, y) {
-        this.ctx = ctx;
-        this.x = x;
-        this.y = y;
-        this.drawables = [];
-    }
-
-    draw () {
-        this.ctx.save();
-        this.ctx.translate(this.x, this.y);
-        this.drawables.forEach((drbl) => {
-            drbl.draw();
-        })
-        this.ctx.restore();
-    }
-}
-
-class Sprite extends Drawable {
-    constructor (ctx, src, x, y, w, h, x_, y_, w_, h_) {
-        w_ ??= w;
-        h_ ??= h;
-        if (x_ !== undefined){
-            super(ctx, x_, y_, w_, h_);
-            this._sx = x;
-            this._sy = y;
-            this._sw = w;
-            this._sh = h;
-        }
-        else {
-            if (w !== undefined){
-                super(ctx, x, y, w, h);
-            }
-            else{
-                super(ctx, x, y);
-                this.w = this.h = undefined
-            }
-            this._sx = this._sy = this._sw = this._sh = undefined;
-        }
-        this.img = new Image();
-        this.img.src = src;
-    }
-
-    get sx () {
-        return this._sx;
-    }
-
-    set sx (value) {
-        this._sx = value;
-    }
-
-    get sy () {
-        return this._sy;
-    }
-
-    set sy (value) {
-        this._sy = value;
-    }
-
-    get sw () {
-        return this._sw;
-    }
-
-    set sw (value) {
-        this._sw = value;
-    }
-
-    get sh () {
-        return this._sh;
-    }
-
-    set sh (value) {
-        this._sh = value;
-    }
-
-    draw () {
-        var mat = this.ctx.getTransform();
-        this.ctx.save();
-        this.ctx.setTransform(
-            1, 0, 0, 
-            1, 0, 0
-        );
-        this.ctx.translate(this.x, this.y);
-        this.ctx.transform(
-            mat.a, mat.b, mat.c,
-            mat.d, mat.e, mat.f
-        );
-        if (this.sx !== undefined) {
-            this.ctx.drawImage(
-                this.img,
-                this.sx, this.sy, this.sw, this.sh,
-                0, 0, this.w, this.h);
-        }
-        else if (this.w !== undefined) {
-            this.ctx.drawImage(
-                this.img,
-                0, 0, this.w, this.h);
-        }
-        else {
-            this.ctx.drawImage(
-                this.img,
-                0, 0);
-
-        }
-        this.ctx.restore();
-    }
-}
-
-class Path extends Drawable {
-    constructor (ctx, x, y, pathData, fill, stroke) {
-        super(ctx, x, y);
-        this.path = new Path2D(pathData);
-        this.fill = fill;
-        this.stroke = stroke;
-    }
-
-    draw () {
-        var mat = this.ctx.getTransform();
-        this.ctx.save();
-        this.ctx.setTransform(
-            1, 0, 0, 
-            1, 0, 0
-        );
-        this.ctx.translate(this.x, this.y);
-        this.ctx.transform(
-            mat.a, mat.b, mat.c,
-            mat.d, mat.e, mat.f
-        );
-        if (this.fill !== null){
-            this.ctx.fillStyle = this.fill;
-            this.ctx.fill(this.path);
-        }
-        if (this.stroke !== null){
-            this.ctx.strokeStyle = this.stroke;
-            this.ctx.stroke(this.path);
-        }
-        this.ctx.restore();
-    }
-}
-
-class GameState extends DrawableGroup {
-    constructor (ctx, x, y) {
-        super(ctx, x, y);
-        this.drawables = [];
-        this.passDraw = false;
-        this.passUpdate = false;
-        this.lastTime = undefined;
-    }
-
-    update (t) {
-        this.lastTime = t;
-    }
-
-    log () {
-        console.log("log", this, ...arguments);
-    }
-}
-Object.assign(GameState.prototype, GameEventTarget);
+function bisect(arr,el){var a=0,z=arr.length,m;for(;;){m=Math.floor((a+z)/2);if(arr[m]==el||a==z)return m;if(arr[m]<el)a=m+1;else z=m}}
+class OptSet{constructor(){this.values=[]}
+add(data){var ind=bisect(this.values,data);if(this.values[ind]!=data||this.values[ind]==undefined){this.values.splice(ind,0,data)}}
+remove(data){var ind=bisect(this.values,data);if(this.values[ind]==data){this.values.splice(ind,1)}}
+toggle(data){var ind=bisect(this.values,data);if(this.values[ind]!=data||this.values[ind]==undefined){this.values.splice(ind,0,data)}else{this.values.splice(ind,1)}}
+has(data){var ind=bisect(this.values,data);return this.values[ind]==data}}
+class Trie{constructor(){this.data=undefined;this.keys=[];this.values=[]}
+set(key,data){if(key.length==0){this.data=data}else{var k,ey;[k,...ey]=key;var ind=bisect(this.keys,k);if(this.keys[ind]!=k||this.keys[ind]==undefined){this.keys.splice(ind,0,k);this.values.splice(ind,0,new Trie())}
+this.values[ind].set(ey,data)}}
+get(key){if(key.length==0){return this.data}else{var k,ey;[k,...ey]=key;var ind=bisect(this.keys,k);if(this.keys[ind]!=k||this.keys[ind]==undefined){return undefined}
+return this.values[ind].get(ey)}}
+forEach(fn,key=[]){if(this.data!==undefined)fn(this.data,key);this.values.forEach((trie,ind)=>{trie.forEach(fn,[...key,this.keys[ind]])})}
+entries(){return{trie:this,*[Symbol.iterator](){if(this.trie.data!=undefined){yield[[],this.trie.data]}
+for(var i=0;i<this.trie.keys.length;i++){for(var[key,val]of this.trie.values[i].entries()){yield[[this.trie.keys[i],...key],val]}}}}}}
+class Drawable{constructor(ctx,x,y,w,h){this.ctx=ctx;this._x=x;this._y=y;this._w=w;this._h=h}
+get x(){return this._x}
+set x(value){this._x=value}
+get y(){return this._y}
+set y(value){this._y=value}
+get w(){return this._w}
+set w(value){this._w=value}
+get h(){return this._h}
+set h(value){this._h=value}
+draw(){}}
+class DrawableGroup{constructor(ctx,x,y){this.ctx=ctx;this.x=x;this.y=y;this.drawables=[]}
+draw(){this.ctx.save();this.ctx.translate(this.x,this.y);this.drawables.forEach((drbl)=>{drbl.draw()})
+this.ctx.restore()}}
+class Sprite extends Drawable{constructor(ctx,src,x,y,w,h,x_,y_,w_,h_){w_??=w;h_??=h;if(x_!==undefined){super(ctx,x_,y_,w_,h_);this._sx=x;this._sy=y;this._sw=w;this._sh=h}else{if(w!==undefined){super(ctx,x,y,w,h)}else{super(ctx,x,y);this.w=this.h=undefined}
+this._sx=this._sy=this._sw=this._sh=undefined}
+this.img=new Image();this.img.src=src}
+get sx(){return this._sx}
+set sx(value){this._sx=value}
+get sy(){return this._sy}
+set sy(value){this._sy=value}
+get sw(){return this._sw}
+set sw(value){this._sw=value}
+get sh(){return this._sh}
+set sh(value){this._sh=value}
+draw(){var mat=this.ctx.getTransform();this.ctx.save();this.ctx.setTransform(1,0,0,1,0,0);this.ctx.translate(this.x,this.y);this.ctx.transform(mat.a,mat.b,mat.c,mat.d,mat.e,mat.f);if(this.sx!==undefined){this.ctx.drawImage(this.img,this.sx,this.sy,this.sw,this.sh,0,0,this.w,this.h)}else if(this.w!==undefined){this.ctx.drawImage(this.img,0,0,this.w,this.h)}else{this.ctx.drawImage(this.img,0,0)}
+this.ctx.restore()}}
+class Path extends Drawable{constructor(ctx,x,y,pathData,fill,stroke){super(ctx,x,y);this.path=new Path2D(pathData);this.fill=fill;this.stroke=stroke}
+draw(){var mat=this.ctx.getTransform();this.ctx.save();this.ctx.setTransform(1,0,0,1,0,0);this.ctx.translate(this.x,this.y);this.ctx.transform(mat.a,mat.b,mat.c,mat.d,mat.e,mat.f);if(this.fill!==null){this.ctx.fillStyle=this.fill;this.ctx.fill(this.path)}
+if(this.stroke!==null){this.ctx.strokeStyle=this.stroke;this.ctx.stroke(this.path)}
+this.ctx.restore()}}
+class GameState extends DrawableGroup{constructor(ctx,x,y){super(ctx,x,y);this.drawables=[];this.passDraw=!1;this.passUpdate=!1;this.lastTime=undefined}
+update(t){this.lastTime=t}}
--- grabby.js
+++ grabby.js
@@ -1,154 +1,17 @@
-class Menu extends GameState {
-    constructor (ctx, x, y, world, map, list) {
-        super(ctx, x, y);
-        this.grid = new Grid(ctx, 0, 0, 50, world);
-        this.world = world;
-        this.grid.register(map, list);
-        this.cursor = 0;
-        this.choices = [];
-        this.drawables = [this.grid];
-    }
-    
-    keydown (ev) {
-        var mod = this.choices.length;
-        if (ev.code == this.world.keys.confirm) {
-            this.choices[this.cursor]();
-        }
-        else if (ev.code == this.world.keys.up) {
-            this.cursor = (this.cursor + mod - 1) % mod;
-        }
-        else if (ev.code == this.world.keys.down) {
-            this.cursor = (this.cursor + 1) % mod;
-        }
-        return false;
-    }
-}
-
-class MainMenu extends Menu {
-    constructor (ctx, world, map, list) {
-        super(ctx, 0, 0, world, map, list);
-        this.choices = [
-            () => {
-                setLevel(0, 7, 4, null, null, null, true);
-            }
-        ];
-        this.drawables = [
-            this.grid,
-            new Sprite(ctx, "img/tileset.png", 400, 550, 600, 100, 50, 50),
-            new Sprite(ctx, "img/tileset.png", 500, 650, 500, 100, 150, 150),
-            new Sprite(ctx, "img/tileset.png", 300, 550, 100, 100, 650, 150),
-            new Sprite(ctx, "img/tileset.png", 0, 500, 250, 50, 200, 300),
-            new Sprite(ctx, "img/tileset.png", 350, 650, 150, 50, 450, 300),
-        ];
-    }
-}
-
-class WinMenu extends Menu {
-    constructor (ctx, world, map, list) {
-        super(ctx, -25, -25, world, map, list);
-        this.player = new Player(ctx, 0, 0, 50);
-        this.player.gx = 8;
-        this.player.gy = 5;
-        this.choices = [
-            () => {
-                states.splice(1);
-            }
-        ];
-        this.drawables = [
-            this.grid,
-            this.player,
-            new Sprite(ctx, "img/tileset.png", 0, 550, 300, 100, 100, 50),
-            new Sprite(ctx, "img/tileset.png", 0, 650, 300, 100, 450, 50),
-            new Sprite(ctx, "img/tileset.png", 0, 500, 250, 50, 200, 150),
-            new Sprite(ctx, "img/tileset.png", 350, 700, 150, 50, 450, 150),
-        ];
-    }
-}
-
-menuGrid = [
-    [["FL0", "WNW"], ["FL1", "WlN"], ["FL0", "WlN"], ["FL1", "WlN"], ["FL0", "WlN"], ["FL1", "WlN"], ["FL0", "WlN"], ["FL1", "WlN"], ["FL0", "WlN"], ["FL1", "WlN"], ["FL0", "WlN"], ["FL1", "WlN"], ["FL0", "WlN"], ["FL1", "WNE"], ["FL0"], ["FL1"], ],
-    [["FL1", "WlW"], ["FL0"], ["FL0"], ["FL1"], ["FL1"], ["FL0"], ["FL0"], ["FL1"], ["FL1"], ["FL0"], ["FL0"], ["FL1"], ["FL1"], ["FL0", "WlE"], ["FL1"], ["FL0"], ],
-    [["FL0", "WlW"], ["FL0"], ["FL0"], ["FL1"], ["FL1"], ["FL0"], ["FL0"], ["FL1"], ["FL1"], ["FL0"], ["FL0"], ["FL1"], ["FL1"], ["FL1", "CSW"], ["FL0", "WlN"], ["FL1", "WNE"], ],
-    [["FL1", "WSW"], ["FL0", "WlS"], ["FL1", "CNE"], ["FL0"], ["FL0"], ["FL1"], ["FL1"], ["FL0"], ["FL0"], ["FL1"], ["FL1"], ["FL0"], ["FL0"], ["FL1"], ["FL1"], ["FL0", "WlE"], ],
-    [["FL0"], ["FL1"], ["FL0", "WlW"], ["FL0"], ["FL0"], ["FL1"], ["FL1"], ["FL0"], ["FL0"], ["FL1"], ["FL1"], ["FL0"], ["FL0"], ["FL1"], ["FL1"], ["FL1", "WlE"], ],
-    [["FL1"], ["FL0"], ["FL1", "WSW"], ["FL0", "WlS"], ["FL1", "WlS"], ["FL0", "WlS"], ["FL1", "WlS"], ["FL0", "WlS"], ["FL1", "WlS"], ["FL0", "WlS"], ["FL1", "WlS"], ["FL0", "WlS"], ["FL1", "WlS"], ["FL0", "WlS"], ["FL1", "WlS"], ["FL0", "WSE"], ],
-    [["FL0"], ["FL1"], ["FL0"], ["FL1", "Tb0"], ["FL0"], ["FL0"], ["FL0"], ["FL0"], ["FL0"], ["FL1"], ["FL1"], ["FL1"], ["FL0", "Tb0"], ["FL1"], ["FL0"], ["FL1"], ],
-    [["FL1"], ["FL0"], ["FL1"], ["FL0", "Sh1"], ["FL1", "Sh2"], ["FL0", "Sh2"], ["FL1", "Sh2"], ["FL0", "Sh2"], ["FL1", "Sh2"], ["FL0", "Sh2"], ["FL1", "Sh2"], ["FL0", "Sh2"], ["FL1", "Sh3"], ["FL0"], ["FL1"], ["FL0"], ],
-    [["FL0"], ["FL1"], ["FL0"], ["FL1"], ["FL0"], ["FL1"], ["FL0"], ["FL1"], ["FL0"], ["FL1"], ["FL0"], ["FL1"], ["FL0"], ["FL1"], ["FL0"], ["FL1"], ],
-];
-
-winGrid = [
-    [["FL0"], ["FL1", "WNW"], ["FL0", "WlN"], ["FL1", "WlN"], ["FL0", "WlN"], ["FL1", "WlN"], ["FL0", "WlN"], ["FL1", "WlN"], ["FL0", "WlN"], ["FL1", "WlN"], ["FL0", "WlN"], ["FL1", "WlN"], ["FL0", "WlN"], ["FL1", "WlN"], ["FL0", "WlN"], ["FL1", "WNE"], ["FL0"], ],
-    [["FL1"], ["FL0", "WlW"], ["FL0"], ["FL0"], ["FL1"], ["FL1"], ["FL0"], ["FL0"], ["FL1"], ["FL1"], ["FL1"], ["FL0"], ["FL0"], ["FL1"], ["FL1"], ["FL0", "WlE"], ["FL1"], ],
-    [["FL0"], ["FL1", "WlW"], ["FL0"], ["FL0"], ["FL1"], ["FL1"], ["FL0"], ["FL0"], ["FL0"], ["FL1"], ["FL1"], ["FL0"], ["FL0"], ["FL1"], ["FL1"], ["FL1", "WlE"], ["FL0"], ],
-    [["FL1"], ["FL0", "WSW"], ["FL1", "WlS"], ["FL0", "CNE"], ["FL1"], ["FL1"], ["FL1"], ["FL1"], ["FL1"], ["FL0"], ["FL0"], ["FL0"], ["FL1", "CNW"], ["FL0", "WlS"], ["FL1", "WlS"], ["FL0", "WSE"], ["FL1"], ],
-    [["FL0"], ["FL1"], ["FL0"], ["FL1"], ["FL0"], ["FL1"], ["FL0"], ["FL1"], ["FL0"], ["FL1"], ["FL0"], ["FL1"], ["FL0"], ["FL1"], ["FL0"], ["FL1"], ["FL0"], ],
-    [["FL1"], ["FL0"], ["FL1"], ["FL0", "Sh5", ["###", 1]], ["FL1", "Sh7", ["###", 2]], ["FL0"], ["FL1"], ["FL0"], ["FL1"], ["FL0"], ["FL1"], ["FL0"], ["FL1", "Sh5",["###", 3]], ["FL0", "Sh7",["###", 4]], ["FL1"], ["FL0"], ["FL1"], ],
-    [["FL0"], ["FL1"], ["FL0"], ["FL1", "Sh9", ["###", 5]], ["FL0", "ShB", ["###", 6]], ["FL1", "Tb0", ["###", 7]], ["FL0"], ["FL1"], ["FL0"], ["FL1"], ["FL0"], ["FL1", "Tb0", ["###", 8]], ["FL0", "Sh9",["###", 9]], ["FL1", "ShB",["###", 10]], ["FL0"], ["FL1"], ["FL0"], ],
-    [["FL1"], ["FL0"], ["FL1"], ["FL0", "Sh9", ["###", 11]], ["FL1", "ShB", ["###", 12]], ["FL0", "Tb1", ["###", 13]], ["FL1", "Tb2", ["###", 14]], ["FL0", "Tb3", ["###", 15]], ["FL1"], ["FL0", "Tb1", ["###", 16]], ["FL1", "Tb2", ["###", 17]], ["FL0", "Tb3", ["###", 18]], ["FL1", "Sh9",["###", 19]], ["FL0", "ShB",["###", 20]], ["FL1"], ["FL0"], ["FL1"], ],
-    [["FL0"], ["FL1"], ["FL0"], ["FL1", "ShD", ["###", 21]], ["FL0", "ShF", ["###", 22]], ["FL1", "Tb1", ["###", 23]], ["FL0", "Tb2", ["###", 24]], ["FL1", "Tb2", ["###", 25]], ["FL0", "Tb2", ["###", 26]], ["FL1", "Tb2", ["###", 27]], ["FL0", "Tb2", ["###", 28]], ["FL1", "Tb3", ["###", 29]], ["FL0", "ShD",["###", 30]], ["FL1", "ShF",["###", 31]], ["FL0"], ["FL1"], ["FL0"], ],
-    [["FL1"], ["FL0"], ["FL1"], ["FL0"], ["FL1"], ["FL0"], ["FL1"], ["FL0"], ["FL1"], ["FL0"], ["FL1"], ["FL0"], ["FL1"], ["FL0"], ["FL1"], ["FL0"], ["FL1"], ],
-];
-
-winMenu = new WinMenu(ctx, world, winGrid, itemList);
-
-let states = [new MainMenu(ctx, world, menuGrid, itemList)];
-
-function say (msg, top, height, time) {
-    var sec = document.getElementById("message"),
-        p = sec.children[0];
-    sec.style.top = top;
-    sec.style.height = height;
-    sec.classList.remove("hide");
-    p.innerHTML = msg;
-    setTimeout(hide, time ?? Math.max(150 * p.innerText.length, 2000));
-}
-
-function hide () {
-    var sec = document.getElementById("message");
-    sec.classList.add("hide");
-}
-
-function main () {
-    var keys = window.localStorage.getItem("grabby-keys");
-    if (keys == null) {
-        window.localStorage.setItem("grabby-keys", JSON.stringify(this.world.keys))
-    }
-    else {
-        this.world.keys = JSON.parse(keys);
-    }
-    run();
-}
-
-function run (t) {
-    n = states.length;
-    if (n) {
-        var start;
-        for (start = n - 1; start >= 0; start--) {
-            if (states[start].passUpdate == false)
-                break;
-        }
-        for (; start < n; start++){
-            states[start].update(t);
-        }
-        for (start = n - 1; start >= 0; start--) {
-            if (states[start].passDraw == false){
-                break;
-            }
-        }
-        for (; start < n; start++){
-            states[start].draw();
-        }
-        window.requestAnimationFrame(run);
-    }
-    else {
-        states.push(new MainMenu);
-    }
-}
-
-document.body.onkeydown = function keydown (ev) {
-    for (start = states.length - 1; start >= 0; start--) {
-        if (states[start].keydown(ev) == false)
-            break;
-    }
-}
+class Menu extends GameState{constructor(ctx,x,y,world,map,list){super(ctx,x,y);this.grid=new Grid(ctx,0,0,50,world);this.world=world;this.grid.register(map,list);this.cursor=0;this.choices=[];this.drawables=[this.grid]}
+keydown(ev){var mod=this.choices.length;if(ev.code==this.world.keys.confirm){this.choices[this.cursor]()}else if(ev.code==this.world.keys.up){this.cursor=(this.cursor+mod-1)%mod}else if(ev.code==this.world.keys.down){this.cursor=(this.cursor+1)%mod}
+return!1}}
+class MainMenu extends Menu{constructor(ctx,world,map,list){super(ctx,0,0,world,map,list);this.choices=[()=>{setLevel(0,7,4,null,null,null,!0)}];this.drawables=[this.grid,new Sprite(ctx,"tileset.png",400,550,600,100,50,50),new Sprite(ctx,"tileset.png",500,650,500,100,150,150),new Sprite(ctx,"tileset.png",300,550,100,100,650,150),new Sprite(ctx,"tileset.png",0,500,250,50,200,300),new Sprite(ctx,"tileset.png",350,650,150,50,450,300),]}}
+class WinMenu extends Menu{constructor(ctx,world,map,list){super(ctx,-25,-25,world,map,list);this.player=new Player(ctx,0,0,50);this.player.gx=8;this.player.gy=5;this.choices=[()=>{states.splice(1)}];this.drawables=[this.grid,this.player,new Sprite(ctx,"tileset.png",0,550,300,100,100,50),new Sprite(ctx,"tileset.png",0,650,300,100,450,50),new Sprite(ctx,"tileset.png",0,500,250,50,200,150),new Sprite(ctx,"tileset.png",350,700,150,50,450,150),]}}
+menuGrid=[[["FL0","WNW"],["FL1","WlN"],["FL0","WlN"],["FL1","WlN"],["FL0","WlN"],["FL1","WlN"],["FL0","WlN"],["FL1","WlN"],["FL0","WlN"],["FL1","WlN"],["FL0","WlN"],["FL1","WlN"],["FL0","WlN"],["FL1","WNE"],["FL0"],["FL1"],],[["FL1","WlW"],["FL0"],["FL0"],["FL1"],["FL1"],["FL0"],["FL0"],["FL1"],["FL1"],["FL0"],["FL0"],["FL1"],["FL1"],["FL0","WlE"],["FL1"],["FL0"],],[["FL0","WlW"],["FL0"],["FL0"],["FL1"],["FL1"],["FL0"],["FL0"],["FL1"],["FL1"],["FL0"],["FL0"],["FL1"],["FL1"],["FL1","CSW"],["FL0","WlN"],["FL1","WNE"],],[["FL1","WSW"],["FL0","WlS"],["FL1","CNE"],["FL0"],["FL0"],["FL1"],["FL1"],["FL0"],["FL0"],["FL1"],["FL1"],["FL0"],["FL0"],["FL1"],["FL1"],["FL0","WlE"],],[["FL0"],["FL1"],["FL0","WlW"],["FL0"],["FL0"],["FL1"],["FL1"],["FL0"],["FL0"],["FL1"],["FL1"],["FL0"],["FL0"],["FL1"],["FL1"],["FL1","WlE"],],[["FL1"],["FL0"],["FL1","WSW"],["FL0","WlS"],["FL1","WlS"],["FL0","WlS"],["FL1","WlS"],["FL0","WlS"],["FL1","WlS"],["FL0","WlS"],["FL1","WlS"],["FL0","WlS"],["FL1","WlS"],["FL0","WlS"],["FL1","WlS"],["FL0","WSE"],],[["FL0"],["FL1"],["FL0"],["FL1","Tb0"],["FL0"],["FL0"],["FL0"],["FL0"],["FL0"],["FL1"],["FL1"],["FL1"],["FL0","Tb0"],["FL1"],["FL0"],["FL1"],],[["FL1"],["FL0"],["FL1"],["FL0","Sh1"],["FL1","Sh2"],["FL0","Sh2"],["FL1","Sh2"],["FL0","Sh2"],["FL1","Sh2"],["FL0","Sh2"],["FL1","Sh2"],["FL0","Sh2"],["FL1","Sh3"],["FL0"],["FL1"],["FL0"],],[["FL0"],["FL1"],["FL0"],["FL1"],["FL0"],["FL1"],["FL0"],["FL1"],["FL0"],["FL1"],["FL0"],["FL1"],["FL0"],["FL1"],["FL0"],["FL1"],],];winGrid=[[["FL0"],["FL1","WNW"],["FL0","WlN"],["FL1","WlN"],["FL0","WlN"],["FL1","WlN"],["FL0","WlN"],["FL1","WlN"],["FL0","WlN"],["FL1","WlN"],["FL0","WlN"],["FL1","WlN"],["FL0","WlN"],["FL1","WlN"],["FL0","WlN"],["FL1","WNE"],["FL0"],],[["FL1"],["FL0","WlW"],["FL0"],["FL0"],["FL1"],["FL1"],["FL0"],["FL0"],["FL1"],["FL1"],["FL1"],["FL0"],["FL0"],["FL1"],["FL1"],["FL0","WlE"],["FL1"],],[["FL0"],["FL1","WlW"],["FL0"],["FL0"],["FL1"],["FL1"],["FL0"],["FL0"],["FL0"],["FL1"],["FL1"],["FL0"],["FL0"],["FL1"],["FL1"],["FL1","WlE"],["FL0"],],[["FL1"],["FL0","WSW"],["FL1","WlS"],["FL0","CNE"],["FL1"],["FL1"],["FL1"],["FL1"],["FL1"],["FL0"],["FL0"],["FL0"],["FL1","CNW"],["FL0","WlS"],["FL1","WlS"],["FL0","WSE"],["FL1"],],[["FL0"],["FL1"],["FL0"],["FL1"],["FL0"],["FL1"],["FL0"],["FL1"],["FL0"],["FL1"],["FL0"],["FL1"],["FL0"],["FL1"],["FL0"],["FL1"],["FL0"],],[["FL1"],["FL0"],["FL1"],["FL0","Sh5",["###",1]],["FL1","Sh7",["###",2]],["FL0"],["FL1"],["FL0"],["FL1"],["FL0"],["FL1"],["FL0"],["FL1","Sh5",["###",3]],["FL0","Sh7",["###",4]],["FL1"],["FL0"],["FL1"],],[["FL0"],["FL1"],["FL0"],["FL1","Sh9",["###",5]],["FL0","ShB",["###",6]],["FL1","Tb0",["###",7]],["FL0"],["FL1"],["FL0"],["FL1"],["FL0"],["FL1","Tb0",["###",8]],["FL0","Sh9",["###",9]],["FL1","ShB",["###",10]],["FL0"],["FL1"],["FL0"],],[["FL1"],["FL0"],["FL1"],["FL0","Sh9",["###",11]],["FL1","ShB",["###",12]],["FL0","Tb1",["###",13]],["FL1","Tb2",["###",14]],["FL0","Tb3",["###",15]],["FL1"],["FL0","Tb1",["###",16]],["FL1","Tb2",["###",17]],["FL0","Tb3",["###",18]],["FL1","Sh9",["###",19]],["FL0","ShB",["###",20]],["FL1"],["FL0"],["FL1"],],[["FL0"],["FL1"],["FL0"],["FL1","ShD",["###",21]],["FL0","ShF",["###",22]],["FL1","Tb1",["###",23]],["FL0","Tb2",["###",24]],["FL1","Tb2",["###",25]],["FL0","Tb2",["###",26]],["FL1","Tb2",["###",27]],["FL0","Tb2",["###",28]],["FL1","Tb3",["###",29]],["FL0","ShD",["###",30]],["FL1","ShF",["###",31]],["FL0"],["FL1"],["FL0"],],[["FL1"],["FL0"],["FL1"],["FL0"],["FL1"],["FL0"],["FL1"],["FL0"],["FL1"],["FL0"],["FL1"],["FL0"],["FL1"],["FL0"],["FL1"],["FL0"],["FL1"],],];winMenu=new WinMenu(ctx,world,winGrid,itemList);let states=[new MainMenu(ctx,world,menuGrid,itemList)];function say(msg,top,height,time){var sec=document.getElementById("message"),p=sec.children[0];sec.style.top=top;sec.style.height=height;sec.classList.remove("hide");p.innerHTML=msg;setTimeout(hide,time??Math.max(150*p.innerText.length,2000))}
+function hide(){var sec=document.getElementById("message");sec.classList.add("hide")}
+function main(){var keys=window.localStorage.getItem("grabby-keys");if(keys==null){window.localStorage.setItem("grabby-keys",JSON.stringify(this.world.keys))}else{this.world.keys=JSON.parse(keys)}
+run()}
+function run(t){n=states.length;if(n){var start;for(start=n-1;start>=0;start--){if(states[start].passUpdate==!1)
+break}
+for(;start<n;start++){states[start].update(t)}
+for(start=n-1;start>=0;start--){if(states[start].passDraw==!1){break}}
+for(;start<n;start++){states[start].draw()}
+window.requestAnimationFrame(run)}else{states.push(new MainMenu)}}
+document.body.onkeydown=function keydown(ev){for(start=states.length-1;start>=0;start--){if(states[start].keydown(ev)==!1)
+break}}
